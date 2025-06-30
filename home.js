8063d3d70be08a497eb10326ff9b09630a3cf853
martynas <martynasu28@gmail.com>
2025-06-30 23:09:08 +0300

    Initial commit

---
+++ home.js
@@ -0,0 +1,33 @@
+function showSidebar() {
+          const sidebar = document.querySelector('.sidebar');
+          sidebar.style.display = 'flex';
+        }
+
+        function hideSidebar() {
+          const sidebar = document.querySelector('.sidebar');
+          sidebar.style.display = 'none';
+        }
+
+        document.addEventListener("DOMContentLoaded", function() {
+  const blogCards = document.querySelectorAll('.blog-container');
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate-in');
+      } else {
+        entry.target.classList.remove('animate-in');
+      }
+    });
+  }, { threshold: 0.2 });
+
+  blogCards.forEach(card => observer.observe(card));
+});
+
+function copyToClipboard(id) {
+  const text = document.getElementById(id).innerText;
+  navigator.clipboard.writeText(text).then(() => {
+    alert("IBAN copied to clipboard!");
+  });
+}
+
+
